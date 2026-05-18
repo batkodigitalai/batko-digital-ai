92dd9b97b00b964a4db3ddde725a11032e657830
batkodigitalai <batko.digital.ai@gmail.com>
2026-05-18 21:42:22 +0200

    Refactor API_URL into LEAD_CONFIG object
    oprava

--- 40_OUTPUT/auto-lead-system/config.js
+++ 40_OUTPUT/auto-lead-system/config.js
@@ -1,1 +1,3 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbwcFA8bRyHnBB_4XlgH5_IMR4IBqUfvTD8vScGZPiuCh0gR5f4Mp_9OjOAw1u3lNEjI/exec";
+window.LEAD_CONFIG = {
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwcFA8bRyHnBB_4XlgH5_IMR4IBqUfvTD8vScGZPiuCh0gR5f4Mp_9OjOAw1u3lNEjI/exec"
+};
